8bea87f5ee6befa92647a2b2f50683a4907401f2
nulLeeKH <i_am@nulleekh.com>
2020-10-28 23:31:22 +0900

    [ADD] github_setup.sh

---
+++ github_setup.sh
@@ -0,0 +1,10 @@
+sudo apt install git vim -y
+
+git config --global user.email "i_am@nulleekh.com"
+git config --global user.name "nulLeeKH"
+git config --global credential.helper "store"
+git config --global core.editor "vim"
+
+echo
+echo ---------Done---------
+echo
