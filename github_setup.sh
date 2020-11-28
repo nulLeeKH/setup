432de6ab25601202616cdcb8b5b0782aa9fd3e5c
nulLeeKH <i_am@nulleekh.com>
2020-11-28 23:12:37 +0900

    [FEATURE] Run dependencies.sh on setup

--- github_setup.sh
+++ github_setup.sh
@@ -1,8 +1,10 @@
+sh dependencies.sh
+
 git config --global user.email "$1"
 git config --global user.name "$2"
 git config --global credential.helper "$3"
 git config --global core.editor "$4"
 
 echo
-echo ---------Done---------
+echo ---------github-setup-done---------
 echo
--- nulleekh_setup.sh
+++ nulleekh_setup.sh
@@ -1,8 +1,10 @@
+sh dependencies.sh
+
 git config --global user.email "i_am@nulleekh.com"
 git config --global user.name "nulLeeKH"
 git config --global credential.helper "store"
 git config --global core.editor "vim"
 
 echo
-echo ---------Done---------
+echo ---------nulleekh-setup-done---------
 echo
