088fa6afa559359db85216de0fd88c102187c6dd
nulLeeKH <i_am@nulleekh.com>
2021-04-03 21:13:48 +0900

    fix: Use sudo in nodejs_remove.sh

--- nodejs_remove.sh
+++ nodejs_remove.sh
@@ -1,5 +1,5 @@
-apt remove nodejs
-rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/npm*
+sudo apt remove nodejs
+sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/npm*
 
 echo
 echo ---------nodejs-remove done---------
